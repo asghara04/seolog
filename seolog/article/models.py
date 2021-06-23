e29347d73f39bf16a413d6abe4b8884bd4d4005e
Asghar Ale <asgharale2021@gmail.com>
2021-06-23 07:25:04 +0430

    README fiels added

--- seolog/article/models.py
+++ seolog/article/models.py
@@ -3,5 +3,31 @@ from django.db import models
 class Article(models.Model):
 	title = models.CharField(
 		max_length=250,
-		verbose_name
+		verbose_name="title"
 	)
+	slug = models.SlugField(
+		max_length=250,
+		verbose_name="slug",
+		unique=True
+	)
+	image = models.ImageField(
+		upload_to="Article Images/%Y/%m",
+		verbose_name="image"
+	)
+	description = models.TextField(
+		max_length=400,
+		verbose_name="description"
+	)
+
+	body = models.TextField(
+		max_length=10000,
+		verbose_name="body"
+	)
+
+	publish_date = models.DateTimeField(auto_now_add=True)
+	update = models.DateTimeField(auto_now=True)
+
+	objects = models.Manager()
+
+	def __str__(self):
+		return self.title
--- seolog/seolog/settings.py
+++ seolog/seolog/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
